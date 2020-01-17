b723bf28230792aceee82ff04d7d29047cb996dd
Mark Allison <mark@stylingandroid.com>
2020-01-17 09:21:09 +0000

    Code for BurgerMenu article

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ const val kotlinVersion = "1.3.61"
 
 object BuildPlugins {
     object Version {
-        const val androidBuildToolsVersion = "4.0.0-alpha06"
+        const val androidBuildToolsVersion = "4.0.0-alpha07"
     }
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Version.androidBuildToolsVersion}"
@@ -24,7 +24,7 @@ object Libraries {
     private object Versions {
         const val appCompat = "1.2.0-alpha01"
         const val ktx = "1.2.0-rc01"
-        const val constraintLayout = "2.0.0-beta3"
+        const val constraintLayout = "2.0.0-beta4"
         const val vectorDrawable = "1.1.0"
     }
 
