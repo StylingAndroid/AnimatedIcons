718972bac36b881255b60a0e66a79b97a754cf26
Mark Allison <mark@stylingandroid.com>
2020-02-08 14:00:10 +0000

    Code for Info article

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
-const val kotlinVersion = "1.3.61"
+const val kotlinVersion = "1.3.70-eap-184"
 
 object BuildPlugins {
     object Version {
-        const val androidBuildToolsVersion = "4.0.0-alpha07"
+        const val androidBuildToolsVersion = "4.0.0-alpha09"
     }
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Version.androidBuildToolsVersion}"
@@ -22,8 +22,8 @@ object AndroidSdk {
 
 object Libraries {
     private object Versions {
-        const val appCompat = "1.2.0-alpha01"
-        const val ktx = "1.2.0-rc01"
+        const val appCompat = "1.2.0-alpha02"
+        const val ktx = "1.3.0-alpha01"
         const val constraintLayout = "2.0.0-beta4"
         const val vectorDrawable = "1.1.0"
     }
