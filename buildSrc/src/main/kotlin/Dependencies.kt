0af58439c9e11afe38e6b5fb6a6392069c524f77
Mark Allison <mark@stylingandroid.com>
2020-05-16 13:40:37 +0100

    Adds code for Infinity article

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
-const val kotlinVersion = "1.4-M1"
+const val kotlinVersion = "1.3.72"
 
 object BuildPlugins {
     object Version {
-        const val androidBuildToolsVersion = "4.1.0-alpha05"
+        const val androidBuildToolsVersion = "4.1.0-alpha09"
     }
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Version.androidBuildToolsVersion}"
@@ -22,10 +22,11 @@ object AndroidSdk {
 
 object Libraries {
     private object Versions {
-        const val appCompat = "1.2.0-beta01"
-        const val ktx = "1.3.0-beta01"
-        const val constraintLayout = "2.0.0-beta4"
-        const val vectorDrawable = "1.1.0"
+        const val appCompat = "1.2.0-rc01"
+        const val ktx = "1.3.0-rc01"
+        const val constraintLayout = "2.0.0-beta6"
+        const val vectorDrawable = "1.2.0-alpha01"
+        const val animatedVectorDrawable = "1.1.0"
     }
 
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion"
@@ -33,5 +34,5 @@ object Libraries {
     const val ktxCore = "androidx.core:core-ktx:${Versions.ktx}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
     const val vectorDrawable = "androidx.vectordrawable:vectordrawable:${Versions.vectorDrawable}"
-    const val animatedVectorDrawable = "androidx.vectordrawable:vectordrawable-animated:${Versions.vectorDrawable}"
+    const val animatedVectorDrawable = "androidx.vectordrawable:vectordrawable-animated:${Versions.animatedVectorDrawable}"
 }
